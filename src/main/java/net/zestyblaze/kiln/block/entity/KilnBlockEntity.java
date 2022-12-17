07b32924aa6eb70a0d1aad23630d75c9c1c2d173
dfsek <dfsek@protonmail.com>
2022-12-16 18:14:25 -0700

    update to 1.19.2

--- src/main/java/net/zestyblaze/kiln/block/entity/KilnBlockEntity.java
+++ src/main/java/net/zestyblaze/kiln/block/entity/KilnBlockEntity.java
@@ -9,7 +9,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
@@ -171,7 +170,7 @@ public class KilnBlockEntity extends BaseContainerBlockEntity implements Worldly
 
     @Override
     protected Component getDefaultName() {
-        return new TranslatableComponent("block.kiln.kiln");
+        return Component.translatable("block.kiln.kiln");
     }
 
     @Override
--- src/main/java/net/zestyblaze/kiln/client/recipebook/KilnBookRecipeScreen.java
+++ src/main/java/net/zestyblaze/kiln/client/recipebook/KilnBookRecipeScreen.java
@@ -2,14 +2,13 @@ package net.zestyblaze.kiln.client.recipebook;
 
 import net.minecraft.client.gui.screens.recipebook.AbstractFurnaceRecipeBookComponent;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 
 import java.util.Set;
 
 public class KilnBookRecipeScreen extends AbstractFurnaceRecipeBookComponent {
-    private static final Component LABEL = new TranslatableComponent("gui.recipebook.toggleRecipes.firable");
+    private static final Component LABEL = Component.translatable("gui.recipebook.toggleRecipes.firable");
 
     @Override
     protected Component getRecipeFilterName() {
